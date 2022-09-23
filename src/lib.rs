ef5c43edc7070e563c8f2ffd6108d7a9039902a1
elapuestojoe <kevin.islas.abud@gmail.com>
2022-09-23 14:27:09 -0700

    Adds Float from i32 method

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,31 @@ use rand::Rng;
 pub trait Float: SubFloat {
     fn random() -> Self;
     fn random_range(low: Self, high: Self) -> Self;
+    fn from_i32(i: i32) -> Self;
 }
 
 impl Float for f32 {
     fn random() -> f32 {
         let mut rng = rand::thread_rng();
-        return rng.gen();
+        rng.gen()
     }
     fn random_range(low: Self, high: Self) -> f32 {
-        return Self::random() * (high - low) + low;
+        Self::random() * (high - low) + low
+    }
+    fn from_i32(i: i32) -> f32 {
+        i as f32
     }
 }
 impl Float for f64 {
     fn random() -> f64 {
         let mut rng = rand::thread_rng();
-        return rng.gen();
+        rng.gen()
     }
     fn random_range(low: Self, high: Self) -> f64 {
-        return Self::random() * (high - low) + low;
+        Self::random() * (high - low) + low
+    }
+    fn from_i32(i: i32) -> f64 {
+        i as f64
     }
 }
 
@@ -51,5 +58,11 @@ mod tests {
     fn f64_random_range() {
         let i: f64 = Float::random_range(-1f64, 1f64);
         assert!(i > -1f64 && i < 1f64);
+    }
+
+    #[test]
+    fn f64_from_i32() {
+        let i: f64 = Float::from_i32(-2);
+        assert!(i == -2f64);
     }
 }
